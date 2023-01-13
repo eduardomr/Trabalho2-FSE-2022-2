8b705b5af19f2bb0bb8ceb3e629154c1a433299d
Eduardo <emaiarezende@gmail.com>
2023-01-13 20:53:49 -0300

    ajuste modbus

--- uart.py
+++ uart.py
@@ -21,15 +21,15 @@ comando = input("Selecione o comando")
 
 if comando == '1':
     codigo_funcao = 0x16
-    subcodigo = b"0xD3 1"
-    mensagem = struct.pack(">BBn", endereco_rasp, codigo_funcao, subcodigo )
+    subcodigo = 0xD3
+    mensagem = struct.pack(">BB4BB", endereco_rasp, codigo_funcao, subcodigo, 9, 2, 3, 1, 1 )
     crc_calculado = crc.calcula_crc(mensagem,len(mensagem))
-    mensagem_crc = struct.pack(">BBnH", endereco_rasp, codigo_funcao,subcodigo, crc_calculado)
+    mensagem_crc = struct.pack(">BB4BBH", endereco_rasp, codigo_funcao,subcodigo, 9,2,3,1,1,crc_calculado)
     ser.write(mensagem_crc)
 if comando == '2':
     codigo_funcao = 0x16
-    subcodigo = b"0xD3 0"
-    mensagem = struct.pack(">BBn", endereco_rasp, codigo_funcao, subcodigo)
+    subcodigo = 0xD3
+    mensagem = struct.pack(">BB4BB", endereco_rasp, codigo_funcao, subcodigo, 9, 2, 3, 1, 0 )
     crc_calculado = crc.calcula_crc(mensagem,len(mensagem))
-    mensagem_crc = struct.pack(">BBnH", endereco_rasp, codigo_funcao, crc_calculado)
+    mensagem_crc = struct.pack(">BB4BBH", endereco_rasp, codigo_funcao,subcodigo, 9,2,3,1,0, crc_calculado)
     ser.write(mensagem_crc)
