4ab1822692d636127fc6fe0504f180c196251fe1
Eduardo <emaiarezende@gmail.com>
2023-01-20 19:49:34 -0300

    pid e pwm

--- gpio.py
+++ gpio.py
@@ -9,4 +9,25 @@ GPIO.setwarnings(False)
 GPIO.setup(ventoinha_pin, GPIO.OUT)
 GPIO.setup(resistor_pin, GPIO.OUT)
 
+resistor_pwm = GPIO.PWM(resistor_pin, 100)
+ventoinha_pwm = GPIO.PWM(ventoinha_pin, 100)
 
+def start_pwm():
+    resistor_pwm.start(0)
+    ventoinha_pwm.start(0)
+
+def stop_pwm():
+    resistor_pwm.stop()
+    ventoinha_pwm.stop()
+
+def set_pwm(ventoinha, resistor):
+    if ventoinha < 35:
+        ventoinha = 35
+    resistor_pwm.ChangeDutyCycle(resistor)
+    ventoinha_pwm.ChangeDutyCycle(ventoinha)
+
+def controle_pwm(referencia):
+    if referencia > 0:
+        set_pwm(0, referencia)
+    else:
+        set_pwm(-referencia, 0)
---
+++ pid.py
@@ -0,0 +1,16 @@
+class PID:
+
+    def __init__(self):
+        self.kp = 30.0
+        self.ki = 0.2
+        self.kd = 400.0
+        self.referencia = 0.0
+        self.erro_anterior = 0.0
+        self.integral = 0.0
+
+    def atualizar_pid(self, erro, valor_atual):
+        erro = self.referencia - valor_atual
+        self.integral += erro
+        derivada = erro - self.erro_anterior
+        self.erro_anterior = erro
+        print( self.kp * erro + self.ki * self.integral + self.kd * derivada )
