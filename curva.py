e10c0f13550c34c123f7d669a05c2cf835be08ba
Eduardo <emaiarezende@gmail.com>
2023-01-21 15:39:02 -0300

    thread para curva

--- curva.py
+++ curva.py
@@ -4,6 +4,7 @@ import struct
 import pid
 import gpio
 import uart
+import threading
 
 
 pid_curva = pid.PID()
@@ -14,23 +15,37 @@ solicita_tmp_interna = [0x01, 0x23, 0xC1, *matricula]
 envia_sinal_referencia = [0x01, 0x16, 0xD2, *matricula] # + Float de valor
 gpio.start_pwm()
 
+def change_params():
+    while True:
+        with open('curva_reflow.csv') as csvfile:
+            csv_reader_thread = csv.reader(csvfile, delimiter=',')
+            csv_reader_thread.__next__()
+            for row in csv_reader_thread:
+                temp_referencia = float(row[1])
+                pid_curva.atualiza_referencia(temp_referencia)
+                uart.envia_recebe(envia_sinal_referencia , temp_referencia)
+                time.sleep(float(row[0]))
+
+thread = threading.Thread(target=change_params)
+thread.start()
+
 def controle_curva():
+    
     with open('curva_reflow.csv') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=',')
         csv_reader.__next__()
 
+        thread = threading.Thread(target=change_params)
+        thread.start()
         for row in csv_reader:
             temp_referencia = float(row[1])
-            uart.envia_recebe(envia_sinal_referencia , temp_referencia)
             resposta = uart.envia_recebe(solicita_tmp_interna)
             temp_interna = resposta
             print("Temperatura interna: ", temp_interna)
             print("Temperatura referencia: ", temp_referencia)
-            pid_curva.atualiza_referencia(temp_referencia)
             valor_pwm = pid_curva.controle(temp_interna)
             gpio.controle_pwm(valor_pwm)
             print("Controle de Sinal: ", valor_pwm)
-            time.sleep(float(row[0]))
 
     gpio.controle_pwm(0.0)
     gpio.stop_pwm()
