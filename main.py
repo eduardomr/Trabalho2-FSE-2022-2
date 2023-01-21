34803acebf49c973f58fc65c664d89ebf162bc82
Eduardo <emaiarezende@gmail.com>
2023-01-21 09:29:10 -0300

    implementação de controle manual na main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import uart
+import gpio
+import pid
+import time
+
+pid.__init__()
+pid.configura_constantes(30.0, 0.2, 400.0)
+pid.atualiza_referencia(0.0)
 
 
 #DEFINIÇÃO DE COMANDOS----------------------
@@ -16,20 +23,25 @@ estado_funcionamento_on = [0x01, 0x23, 0xD5, *matricula, 1]
 estado_funcionamento_off = [0x01, 0x23, 0xD5, *matricula, 0]
 #-------------------------------------------
 modo="manual"
+estado_forno = [0,0]
 while True:
     cod, info = uart.recebe_resposta()
     if cod == 0xA1:
         print("Comando de ligar recebido")
         uart.envia_recebe(estado_forno_on)
+        estado_forno[0]=1
     if cod == 0xA2:
         print("Comando de desligar recebido")
-        uart.envia_recebe(estado_forno_off) 
+        uart.envia_recebe(estado_forno_off)
+        estado_forno[0]=0
     if cod == 0xA3:
         print("Comando de Iniciar Aquecimento Recebido")
         uart.envia_recebe(estado_forno_on)
+        estado_forno[1]=1
     if cod == 0xA4:
         print("Comando de Parar Aquecimento Recebido")
         uart.envia_recebe(estado_forno_off)
+        estado_forno[1]=0
     if cod == 0xA5:
         print("Comando Curva Recebido")
         if modo == "manual":
@@ -39,3 +51,16 @@ while True:
             modo = "manual"
             uart.envia_recebe(modo_manual)
     
+    if estado_forno[0]==1 and estado_forno[1]==1:
+        gpio.start_pwm()
+        if modo == "manual":
+            #Ler temp ambiente tbm
+            cod, temp_ref = uart.envia_recebe(solicita_tmp_referencia)
+            pid.atualiza_referencia(temp_ref)
+            cod, temp_int = uart.envia_recebe(solicita_tmp_interna)
+            gpio.controle_pwm(pid.controle(temp_int))  
+        else:
+            #MODO CURVA
+            continue
+    
+    time.sleep(0.5)
