e9912beb5ef17f9336e515f87d7a611866c59dc5
Eduardo <emaiarezende@gmail.com>
2023-01-21 09:06:28 -0300

    recebendo comandos na main

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 import uart
 
+
+#DEFINIÇÃO DE COMANDOS----------------------
+matricula = [9,2,3,1]
+estado_forno_on = [0x01, 0x16, 0xD3, *matricula, 1]
+estado_forno_off = [0x01, 0x16, 0xD3, *matricula, 0]
+solicita_tmp_interna = [0x01, 0x23, 0xC1, *matricula]
+solicita_tmp_referencia = [0x01, 0x23, 0xC2, *matricula]
+le_cmd_usuario = [0x01, 0x23, 0xC3, *matricula]
+envia_sinal_controle = [0x01, 0x16, 0xD1, *matricula] # + Int de valor
+envia_sinal_referencia = [0x01, 0x16, 0xD2, *matricula] # + Float de valor
+modo_manual = [0x01, 0x16, 0xD4, *matricula, 0]
+modo_curva = [0x01, 0x16, 0xD4, *matricula, 1]
+estado_funcionamento_on = [0x01, 0x23, 0xD5, *matricula, 1]
+estado_funcionamento_off = [0x01, 0x23, 0xD5, *matricula, 0]
+#-------------------------------------------
+modo="manual"
 while True:
     cod, info = uart.recebe_resposta()
     if cod == 0xA1:
         print("Comando de ligar recebido")
+        uart.envia_recebe(estado_forno_on)
+    if cod == 0xA2:
+        print("Comando de desligar recebido")
+        uart.envia_recebe(estado_forno_off) 
+    if cod == 0xA3:
+        print("Comando de Iniciar Aquecimento Recebido")
+        uart.envia_recebe(estado_forno_on)
+    if cod == 0xA4:
+        print("Comando de Parar Aquecimento Recebido")
+        uart.envia_recebe(estado_forno_off)
+    if cod == 0xA5:
+        print("Comando Curva Recebido")
+        if modo == "manual":
+            modo = "curva"
+            uart.envia_recebe(modo_curva)
+        else:
+            modo = "manual"
+            uart.envia_recebe(modo_manual)
     
