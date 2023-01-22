323ec878db9d07f2f9e0a77d4923a687e36f863c
Eduardo <emaiarezende@gmail.com>
2023-01-22 17:57:51 -0300

    .

--- curva.py
+++ curva.py
@@ -48,7 +48,7 @@ def controle_curva():
     gpio.controle_pwm(valor_pwm)
     print("Controle de Sinal: ", valor_pwm)
     uart.envia_comando(envia_sinal_controle, int(valor_pwm))
-    print(i2c.temp_ambiente())
+    print("Temperatura Ambiente: " (round(i2c.temp_ambiente(),2)))
             
 
 """ Tempo (s), Temperatura
